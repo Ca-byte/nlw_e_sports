3857625e453ef028cb9634aa376db71379159931
Caroline Vieira <caroline@fetch.ly>
2022-09-26 20:01:48 +0200

    assets and tailwind config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import './styles/main.css'
+import logoImg from "./assets/logo-nlw-esports.svg"
 
 
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="" />
+      <h1 className='text-6xl text-white font-black mt-20'>Your perfect <span className='bg-nlw-gradient'>duo</span> is here</h1>
+    </div>
       
   )
 }
