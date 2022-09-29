ff5179cc3b7e49b2b66784a6bb7d0d921a6ffbfc
Caroline Vieira <caroline@fetch.ly>
2022-09-29 03:48:49 +0200

    Sqlite and prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,28 @@
-import express  from "express";
+import express, { response }  from "express";
 
 const app = express()
 
-app.get('/eu', (req, res)=> {
+app.get('/games', (request, response)=> {
+    return response.json([])
+})
+
+app.post('/ads', (request, response)=> {
+    return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (req, res)=> {
+    //const gameId = req.params.id;
     return res.json([
         {id: 1, name: 'Doc 1'},
         {id: 2, name: 'Doc 2'},
         {id: 3, name: 'Doc 3'},
         {id: 3, name: 'Doc 4'}
     ])
+})
+
+app.get('/ads/:id/discord', (req, res)=> {
+    //const adId = req.params.id;
+    return res.json([])
 })
 
 app.listen(3333)
